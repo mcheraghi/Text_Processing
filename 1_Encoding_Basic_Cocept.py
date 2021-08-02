4a75cd5adaac89d147eec7156cd627c3e84857c9
mcheraghi <msn.cheraghi@gmail.com>
2021-08-02 10:18:43 +0200

    files added

---
+++ 1_Encoding_Basic_Cocept.py
@@ -0,0 +1,71 @@
+vocab = {}  # maps word to integer representing it
+word_encoding = 1
+
+
+#------------------------------------------------------------------------------------Bag of words: just the word and number of repetition
+def bag_of_words(text):
+  global word_encoding
+
+  words = text.lower().split(" ")  # create a list of all of the words in the text, well assume there is no grammar in our text for this example
+  bag = {}  # stores all of the encodings and their frequency
+
+  for word in words:
+    if word in vocab:
+      encoding = vocab[word]  # get encoding from vocab
+    else:
+      vocab[word] = word_encoding
+      encoding = word_encoding
+      word_encoding += 1
+    
+    if encoding in bag:
+      bag[encoding] += 1
+    else:
+      bag[encoding] = 1
+  
+  return bag
+
+text = "this is a test to see if this test will work is is test a a"
+bag = bag_of_words(text)
+print("Bag:",bag)
+print("word list",vocab)
+
+positive_review = "I thought the movie was going to be bad but it was actually amazing"
+negative_review = "I thought the movie was going to be amazing but it was actually bad"
+
+positive_review = "I thought the movie was going to be bad but it was actually amazing"
+negative_review = "I thought the movie was going to be amazing but it was actually bad"
+
+pos_bag = bag_of_words(positive_review)
+neg_bag = bag_of_words(negative_review)
+
+print("Positive:", pos_bag)
+print("word list",vocab)
+print("Negative:", neg_bag)
+print("word list",vocab)
+
+
+
+#------------------------------------------------------------------------------------Integer encoding:considering the orders
+
+
+def one_hot_encoding(text):
+  global word_encoding
+
+  words = text.lower().split(" ") 
+  encoding = []  
+
+  for word in words:
+    if word in vocab:
+      code = vocab[word]  
+      encoding.append(code) 
+    else:
+      vocab[word] = word_encoding
+      encoding.append(word_encoding)
+      word_encoding += 1
+  
+  return encoding
+
+text = "this is a test to see if this test will work is is test a a"
+encoding = one_hot_encoding(text)
+print(encoding)
+print(vocab)
